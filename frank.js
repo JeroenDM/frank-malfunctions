89767227a66ee1a00e51b92a0d9a24c7e92f8836
jeroendm <jeroendemaeyer@live.be>
2021-03-25 10:08:30 +0100

    add more quotes

--- frank.js
+++ frank.js
@@ -52,5 +52,9 @@ const quotes = [
     "Human beings, who are almost unique in having the ability to learn from the experience of others, are also remarkable for their apparent disinclination to do so.",
     "Life... Don't talk to me about life.",
     "It must be a Thursday. I never could get the hang of Thursdays.",
-    "It hung in the air in exactly the way that bricks don't."
+    "It hung in the air in exactly the way that bricks don't.",
+    "Time is an illusion -- lunchtime doubly so.",
+    "A common mistake that people make when trying to design something completely foolproof is to underestimate the ingenuity of complete fools.",
+    "The Hollywood process is like trying to grill a steak by having a succession of people coming into the room and breathing on it.",
+    "Here's a warm welcome to all the intelligent life forms out there. And to the rest of you... the trick is to bang the rocks together, guys."
 ]
